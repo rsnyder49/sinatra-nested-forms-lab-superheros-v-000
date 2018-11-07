0db3393e76603ed5447520ab3cb351c5f0405ea0
Robert Snyder <r.snyder49@gmail.com>
2018-11-07 23:09:23 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,9 +2,9 @@ class Team
   attr_accessor :name, :motto
   @@teams = []
   
-  def initialize(args)
-    @name = args[:name]
-    @motto = args[:motto]
+  def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
     @@teams << self
   end
   
