0c7a0daf1eeb8cb7b9e8a7faa1beb56a7f30a0bd
Robert Snyder <r.snyder49@gmail.com>
2018-11-07 22:55:43 +0000

    automatically backed up by learn

---
+++ app/models/heroes.rb
@@ -0,0 +1,20 @@
+class Hero
+  attr_accessor :name, :power, :bio
+  @@heros = []
+  
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    @@heros << self
+  end
+  
+  def self.all
+    @@heros
+  end
+  
+  def self.clear
+    @@heros.clear
+  end
+  
+end 
